e01fa2d3c71d69cc8d45d1baf2f9420084e3a744
Marta Gladysz <gladysz.mrt@gmail.com>
2026-06-26 17:05:29 +0200

    feat(module-4): add volatile counter example

---
+++ module-4/src/main/java/com/gladysz/counter/CounterApp.java
@@ -0,0 +1,55 @@
+package com.gladysz.counter;
+
+public class CounterApp {
+
+    private static volatile int counter = 0;
+
+    public static void main(String[] args) {
+
+        final Thread listener = createListener();
+        final Thread changer = createChanger();
+
+        listener.start();
+        changer.start();
+    }
+
+
+    private static Thread createListener() {
+
+        return new Thread(() -> {
+
+            int local_value = counter;
+
+            while (local_value < 5) {
+                if (local_value != counter) {
+                    System.out.println("Received counter change: " + counter);
+                    local_value = counter;
+                }
+            }
+        });
+    }
+
+
+    private static Thread createChanger() {
+
+        return new Thread(() -> {
+
+            int local_value = counter;
+
+            while (counter < 5) {
+                System.out.println("Incrementing counter to " + (counter + 1));
+                counter = ++local_value;
+                try {
+                    Thread.sleep(500);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            }
+        });
+    }
+}
+
+
+
+
+
