4eed7db773bfd1da5b07caf30b058a19ce49466f
Marta Gladysz <gladysz.mrt@gmail.com>
2026-06-30 21:53:56 +0200

    feat(module-4): implement deadlock exercise

---
+++ module-4/src/main/java/com/gladysz/deadlock/Bank.java
@@ -0,0 +1,14 @@
+package com.gladysz.deadlock;
+
+public class Bank {
+    private static final Object lock = new Object();
+
+
+    public void transfer(BankAccount from, BankAccount to, double amount) {
+
+        synchronized (lock) {
+            from.withdraw(amount);
+            to.deposit(amount);
+        }
+    }
+}
---
+++ module-4/src/main/java/com/gladysz/deadlock/BankAccount.java
@@ -0,0 +1,28 @@
+package com.gladysz.deadlock;
+
+public class BankAccount {
+    private final String accountNumber;
+    private double balance;
+
+    public BankAccount(String accountNumber, double balance) {
+        this.accountNumber = accountNumber;
+        this.balance = balance;
+    }
+
+
+    void deposit(double amount) {
+        System.out.println("Deposit " + amount);
+        balance += amount;
+    }
+
+
+    void withdraw(double amount) {
+        System.out.println("Withdraw " + amount);
+        try {
+            Thread.sleep(2000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        balance -= amount;
+    }
+}
